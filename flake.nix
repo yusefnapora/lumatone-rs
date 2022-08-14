c794c627a1060cb96724d4368a571b2e3c9cd4d0
Yusef Napora <yusef@napora.org>
2022-08-14 16:57:45 -0400

    add flake.nix for devShell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system: 
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # TODO: depend on rustc and cargo instead... bit fiddly tho
+          # note that after installing standard toolchain, you also need to run
+          # rustup target add wasm32-unknown-unknown
+          rustup
+
+          # native deps for tauri / dioxus
+          webkitgtk
+          gtk3
+          libayatana-appindicator
+          openssl.dev
+          pkg-config
+        ];
+      };
+
+      # TODO: add package derivation for app, etc
+    });
+}
