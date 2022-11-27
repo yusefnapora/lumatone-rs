7549a8fb9e9b1096578a5a40dd2a6a54fef5b178
Yusef Napora <yusef@protocol.ai>
2022-11-26 20:38:48 -0500

    copy some code in from lumachromatic

---
+++ gui/src/lib/Palette.ts
@@ -0,0 +1,98 @@
+import type { HexColor } from './drawing'
+import RXB from './RXB'
+import type { RXBArray } from './RXB'
+
+import { Note } from '@tonaljs/tonal'
+import type { Scale } from '@tonaljs/scale'
+
+import tinycolor from 'tinycolor2'
+
+// TODO: probably ditch this in favor of color manipulation in rust, who just sends us hex values for display
+
+export default class Palette {
+  divisions: number
+  rainbow: RXBArray[]
+  constructor(divisions: number = 12) {
+    this.divisions = divisions
+    this.rainbow = RXB.rainbow(divisions)
+  }
+
+  primary(index: number): HexColor {
+    index = index % this.rainbow.length
+    return toHex(this.rainbow[index])
+  }
+
+  complementary(index: number, value: number): HexColor {
+    index = index % this.rainbow.length
+    const p = this.rainbow[index]
+    return toHex(RXB.complementary(p, value))
+  }
+
+  neutrals(index: number, value: number, count?: number): HexColor[] {
+    index = index % this.rainbow.length
+    const p = this.rainbow[index]
+    return RXB.neutrals(p, value, count).map(toHex)
+  }
+
+  noteColors(note: string): {
+    primary: HexColor
+    muted: HexColor
+    complementary: (value: number) => HexColor
+  } {
+    const n = Note.get(note)
+    if (!n || n.chroma == null) {
+      throw new Error('unknown note ' + note)
+    }
+
+    const index = n.chroma
+    const primary = this.primary(index)
+    const hsla = tinycolor(primary).toHsl()
+    hsla.l *= 0.3
+    const muted = tinycolor(hsla).toHexString()
+
+    return {
+      primary,
+      muted,
+      complementary: (value) => this.complementary(index, value),
+    }
+  }
+
+  colorForNoteName(
+    noteName: string,
+    scale: Scale | undefined = undefined
+  ): HexColor | undefined {
+    const note = Note.get(noteName)
+    if (!note || note.chroma == null) {
+      return undefined
+    }
+    let muted = false
+    let color = this.primary(note.chroma)
+    if (scale) {
+      muted = true
+      for (const sn of scale.notes) {
+        if (Note.get(sn).chroma === note.chroma) {
+          muted = false
+          break
+        }
+      }
+    }
+    if (muted) {
+      const hsla = tinycolor(color).toHsl()
+      hsla.s *= 0.7
+      hsla.l *= 0.2
+      const c = tinycolor(hsla).toHexString()
+      return c
+    }
+    return this.primary(note.chroma)
+  }
+}
+
+function toHex(color: RXBArray | string): HexColor {
+  if (typeof color === 'string') {
+    if (!color.startsWith('#')) {
+      return '#' + color
+    }
+    return color
+  }
+  return '#' + RXB.rxb2hex(RXB.ryb2rgb(color))
+}
