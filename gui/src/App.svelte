afdf665e4c153761d538664cc72ce96a20c96667
Yusef Napora <yusef@protocol.ai>
2022-11-26 20:56:57 -0500

    wip: color wheel

--- gui/src/App.svelte
+++ gui/src/App.svelte
@@ -2,6 +2,7 @@
 <script>
 	import { AppShell, GradientHeading } from '@brainandbones/skeleton';
     import RailNav from "./ui/RailNav.svelte";
+    import Wedge from "./ui/wheel/Wedge.svelte";
 </script>
 
 <AppShell slotSidebarLeft="bg-surface-500/5">
@@ -21,5 +22,8 @@
     <GradientHeading tag="h2" direction="bg-gradient-to-r" from="from-red-500" to="to-violet-500">
       Preset Editor
     </GradientHeading>
+    <svg width="300" height="300">
+      <Wedge label="C" radius={150} color="teal" center={{x: 150, y: 150 }}/>
+    </svg>
   </div>
 </AppShell>
