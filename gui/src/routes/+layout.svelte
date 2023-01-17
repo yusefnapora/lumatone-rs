4a144a49f486902621600fa0680a572ac9390a03
Yusef Napora <yusef@napora.org>
2023-01-17 17:12:32 -0500

    add some simple pages & wire up routes to rail nav

--- gui/src/routes/+layout.svelte
+++ gui/src/routes/+layout.svelte
@@ -2,6 +2,22 @@
 	import '@skeletonlabs/skeleton/themes/theme-modern.css';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
-</script>
 
-<slot />
+  import {AppShell, GradientHeading} from '@skeletonlabs/skeleton';
+  import RailNav from "../ui/RailNav.svelte";</script>
+
+<AppShell slotSidebarLeft="bg-surface-500/5">
+  
+  <!-- Header -->
+  <svelte:fragment slot="header">
+  </svelte:fragment>
+
+  <!-- Sidebar -->
+  <svelte:fragment slot="sidebarLeft">
+    <RailNav/>
+  </svelte:fragment>
+
+  <div id="page" class="container mx-auto p-8 space-y-8">
+		<slot />  
+	</div>
+</AppShell>
---
+++ gui/src/routes/edit/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import {GradientHeading} from '@skeletonlabs/skeleton';
+  import Wheel from "../../ui/wheel/Wheel.svelte";
+</script>
+
+
+<GradientHeading tag="h2" direction="bg-gradient-to-r" from="from-red-500" to="to-violet-500">
+  Preset Editor
+</GradientHeading>
+<Wheel />
