199a9dfbd94c002eec849299f6a6f2de4bc24b14
Yusef Napora <yusef@napora.org>
2023-02-10 12:21:44 -0500

    rm tauri stuff

--- gui/src/routes/setlists/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { GradientHeading } from '@skeletonlabs/skeleton'
-  import Wheel from '../../ui/wheel/Wheel.svelte'
-</script>
-
-<GradientHeading tag="h2" direction="bg-gradient-to-r" from="from-red-500" to="to-violet-500">
-  Setlists
-</GradientHeading>
-<Wheel />
