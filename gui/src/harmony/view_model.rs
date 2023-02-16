81a036be812835f647236d59ebb40dc5aa878432
Yusef Napora <yusef@napora.org>
2023-02-16 13:46:37 -0500

    refactor: move tuning::view_model mod to its own file

---
+++ gui/src/harmony/view_model.rs
@@ -0,0 +1,92 @@
+//! WIP view models for tuning & scales. needs a lot of revision to fully cover the domain
+use crate::drawing::color::ColorPalette;
+use palette::LinSrgb;
+
+#[derive(PartialEq)]
+pub struct PitchClass {
+  name: String,
+  // TODO: add optional enharmonic name(s)
+}
+
+impl PitchClass {
+  pub fn name(&self) -> &str {
+    &self.name
+  }
+}
+
+#[derive(PartialEq)]
+pub struct Tuning {
+  pub name: String,
+  pitch_classes: Vec<PitchClass>,
+  palette: ColorPalette,
+}
+
+impl Tuning {
+  pub fn new(name: String, pitch_classes: Vec<PitchClass>) -> Tuning {
+    let palette = ColorPalette::default_gradient(pitch_classes.len());
+    Tuning {
+      name,
+      pitch_classes,
+      palette,
+    }
+  }
+
+  pub fn edo_12() -> Tuning {
+    let name = "12 EDO";
+    let pitch_classes = vec![
+      PitchClass {
+        name: String::from("C"),
+      },
+      PitchClass {
+        name: String::from("C#"),
+      },
+      PitchClass {
+        name: String::from("D"),
+      },
+      PitchClass {
+        name: String::from("D#"),
+      },
+      PitchClass {
+        name: String::from("E"),
+      },
+      PitchClass {
+        name: String::from("F"),
+      },
+      PitchClass {
+        name: String::from("F#"),
+      },
+      PitchClass {
+        name: String::from("G"),
+      },
+      PitchClass {
+        name: String::from("G#"),
+      },
+      PitchClass {
+        name: String::from("A"),
+      },
+      PitchClass {
+        name: String::from("A#"),
+      },
+      PitchClass {
+        name: String::from("B"),
+      },
+    ];
+    Tuning::new(String::from(name), pitch_classes)
+  }
+
+  pub fn divisions(&self) -> usize {
+    self.pitch_classes.len()
+  }
+
+  pub fn get_pitch_class(&self, index: usize) -> &PitchClass {
+    &self.pitch_classes[index]
+  }
+
+  pub fn get_color(&self, index: usize) -> LinSrgb {
+    self.palette.get(index)
+  }
+
+  pub fn get_text_color(&self, index: usize) -> LinSrgb {
+    self.palette.get_text_color(index)
+  }
+}
